5e259677e4b32ce9eeb6239c038412557ea8dbb0
MirianFabiolaGuzman <fabiolaguzmanmoreno25@gmail.com>
2024-11-08 16:15:34 -0600

    Ejercicio3

---
+++ Ejercicio_3.java
@@ -0,0 +1,15 @@
+// Mirian Fabiola Guzman Moreno
+// NC: 202410050105
+// Programa que indica si la evaluacion de estas variables es verdadera o falsa (true)(false) 
+public class Ejercicio_3
+{
+    public static void main(String[] args)
+    {
+        int M = 6, T = 1, K = -10;
+
+        System.out.println("M > T: " + (M > T)); 
+        System.out.println("T / K == -5: " + (T / K == -5)); 
+        System.out.println("(M+T == 7) || (M-T == 5): " + ((M + T == 7) || (M - T == 5)));
+        
+    }
+}
