e28b97cb32c200d28d24764111fb2f1e0ffb7e1b
MirianFabiolaGuzman <fabiolaguzmanmoreno25@gmail.com>
2024-11-08 16:15:03 -0600

    Ejercicio2

---
+++ Ejercicio_2.java
@@ -0,0 +1,17 @@
+// Mirian Fabiola Guzman Moreno
+// NC: 202410050105
+// Operaciones aritmeticas de dos numeros enteros 
+public class Ejercicio_2
+{
+    public static void main(String[] args)
+    {
+        int n1 = 10;
+        int n2 = 30;
+
+        System.out.println("Suma: " + (n1 + n2));
+        System.out.println("Resta: " + (n1 - n2));
+        System.out.println("Multiplicacion: " + (n1 * n2));
+        System.out.println("Division: " + (n1 / n2));
+        System.out.println("Modulo: " + (n1 % n2));
+    }
+}
