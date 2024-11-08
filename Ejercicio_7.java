04c4dfaf2c5a4d084364a466db595b16ce9b68fe
MirianFabiolaGuzman <fabiolaguzmanmoreno25@gmail.com>
2024-11-08 16:17:53 -0600

    Ejercicio7

---
+++ Ejercicio_7.java
@@ -0,0 +1,13 @@
+// Mirian Fabiola Guzman Moreno
+// 202410050105
+// Programa que imprime mediante un ciclo los numeros pares del 2 a 100
+public class Ejercicio_7 
+{
+    public static void main(String[] args) 
+    {
+        for (int i = 2 ; i <= 100 ; i += 2)
+        {
+            System.out.println(i);
+        }
+    }
+}
