bbc46e4e6803206077c1dd32cdb30bf8fbc21f5e
MirianFabiolaGuzman <fabiolaguzmanmoreno25@gmail.com>
2024-11-08 16:14:08 -0600

    Ejercicio1

---
+++ Ejercicio_1.java
@@ -0,0 +1,10 @@
+// Mirian Fabiola Guzman Moren0
+// NC: 202410050105
+// Programa que imprime mi nombre
+public class Ejercicio_1
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" Hola , soy Mirian Fabiola Guzman Moreno");
+    }
+}
